dd27dfaef7084104b867a07d098e162b030cca62
Topper <142811385+topperf15@users.noreply.github.com>
2024-04-16 08:07:18 -0400

    Update GPN.py

--- GPN.py
+++ GPN.py
@@ -5,8 +5,35 @@ Created on Wed Apr 10 20:20:47 2024
 
 @author: andrew_s
 """
-from Place import *
-from Trans import *
+class Place(object):
+    '''!
+    Class to represent a place or container in petri nets.
+    '''
+    def __init__(self, name, mark):
+        '''!
+        Contructor method.
+        
+        @param name string: name of the place/container
+        '''
+        self.name = str(name)
+        self.mark = int(mark)
+        self.timer = []
+        self.delay = 0
+        self.attributes = {}
+        
+class Trans(object):
+    '''!
+    Class to represent a transition in petri nets.
+    '''
+    def __init__(self, name):
+        '''!
+        Contructor method.
+        
+        @param name string: name of the transition
+        '''
+        self.name = str(name)
+        self.attributes = {}
+        self.functions = {}
 
 class GPN(object):
     '''!
@@ -20,56 +47,76 @@ class GPN(object):
         self.trans = {}
         self.markings = {}
     
-    def add_places(self, place_name, tokens):
+    def add_places(self, place_name, tokens, delay=0):
         '''!
         Method to add a place/container into the Petri Net.
         '''
         self.places[place_name] = Place(place_name,tokens)
+        self.places[place_name].timer = [0]*tokens
 
-    def add_trans(self, trans_name, attrib):
+    def add_trans(self, trans_name, attrib, func):
         '''!
-        Method to add a place/container into the Petri Net.
+        Method to add a tranition into the Petri Net.
         '''
         self.trans[trans_name] = Trans(trans_name)
         self.trans[trans_name].attributes = attrib
+        self.trans[trans_name].functions = func
         
-    def _get_marking(self, clock):
+    def _get_marking(self):
         marking = {}
+        # a_marking = {}
         for name in self.places.keys():
             marking[name] = self.places[name].mark
-            #print(name,net.places[name].mark)
-        self.markings[str(clock)] = list(marking.values())
-        marking_prime = marking.copy()
-        return marking_prime, marking
-    
-    def _set_marking(self, marking_prime, clock):
-        for name in self.places.keys():
-            self.places[name].mark = marking_prime[name]
-            #print(name,self.places[name].mark)
-        self.markings[str(clock)] = list(marking_prime.values())
+            # timer = self.places[name].timer
+            # delay = self.places[name}.delay
+            # a_mark = len([t for t in timer if t>=delay]) #num that have timed out - available
+            # a_marking[name] = a_mark
         
-    def _step(self,marking_prime,marking):
+        return marking  # also return avail marking (a_marking)
+    
+    def _set_marking(self, delta):
+        for name in self.places.keys():
+            self.places[name].mark += delta[name]
+        #self._store_marking(clock, marking_prime)
+        
+    def _store_marking(self, clock, marking):
+        self.markings[str(clock)] = list(marking.values())
+        
+    def _step(self,marking):
+        # loop through the transitions by name
+        delta = {}
+        for name in self.places.keys():
+            delta[name] = 0
         for tname in self.trans.keys():
-            x = self.trans[tname]
-            pin = x.attributes['in']
-            pout = x.attributes['out']
+            x = self.trans[tname]       # transition
+            pin = x.attributes['in']    # input places list
+            pout = x.attributes['out']  # output places list
+            fin = x.functions['in']     # value of input arcs
+            fout = x.functions['out']   # value at output arcs
             
+            # check to see if there are enough avail tokens
+            # at each input place to fire the transtion (True)
+            test = True
             for name in pin:
-                test = True
-                test = test and marking[name] > 0
+                test = test and marking[name] >= fin[pin.index(name)]
+                #print(marking[name],fin[pin.index(name)],test)
             if test==True:
                 for name in pin:
-                    marking_prime[name] -= 1
+                    delta[name] -= fin[pin.index(name)]
                     #print(marking_prime[name])
                 for name in pout:
-                    marking_prime[name] += 1
-        return marking_prime
+                    delta[name] += fout[pout.index(name)]
+        return delta
     
     def simulate(self, steps):
-        
-        clock = 0
-        while clock < steps:
-            marking_prime, marking = self._get_marking(clock)
-            marking_prime = self._step(marking_prime, marking)
-            self._set_marking(marking_prime, clock)
+        # get and store and intial marking
+        marking = self._get_marking()
+        self._store_marking(0, marking)
+        clock = 1
+        while clock <= steps:
+            marking = self._get_marking()
+            marking_prime = marking.copy()
+            delta = self._step(marking)
+            self._set_marking(delta)
+            self._store_marking(clock, marking_prime)
             clock += 1
